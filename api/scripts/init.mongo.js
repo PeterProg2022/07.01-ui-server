3608c64f0c4625c1387e025867b233d4e1b2b015
Peter Prog <peter.prog@yahoo.com>
2022-08-05 09:14:21 +0800

    db remote ok (einmalig CORS error)

--- api/scripts/init.mongo.js
+++ api/scripts/init.mongo.js
@@ -22,6 +22,16 @@ const issuesDB = [
     created: new Date('2019-01-16'), due: new Date('2019-02-01'),
     title: 'Missing bottom border on panel',
   },
+  {
+    id: 3, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+    title: 'Missing bottom border on panel',
+  },
+  {
+    id: 4, status: 'Assigned', owner: 'Eddie', effort: 14,
+    created: new Date('2019-01-16'), due: new Date('2019-02-01'),
+    title: 'Missing bottom border on panel',
+  },
 ];
 
 db.issues.insertMany(issuesDB);
